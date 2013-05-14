9a31cac2eee8cfc9336fbc715185d9567c6cd51c
Nikita Repin <nikita@nulana.com>
2013-05-14 19:15:17 +0600

    init

---
+++ mczn59/models.py
@@ -0,0 +1,38 @@
+# ~*~ coding: utf-8 ~*~
+
+from django.db import models
+from django.contrib import admin
+
+import datetime
+from django.utils import timezone
+
+# Create your models here.
+
+GENDER = (('none', 'Не определен'), ('male', 'Мужчина'), ('female', 'Женщина'))
+CITY = (('perm', 'Пермь'), ('krasnokamsk', 'Краснокамск'), ('berez', 'Березники'), ('dobr', 'Добрянка'), ('pol', 'Полазна'))
+DISTRICT = (('dz', 'Дзержинский'), ('in', 'Индустриальный'), ('ki', 'Кировский'), ('le', 'Ленинский'), ('mo', 'Мотовилихинский'), ('or', 'Орджоникидзевский'), ('sv', 'Свердловский'))
+
+class User(models.Model):
+	username = models.CharField(unique=True, max_length=100)
+	phone = models.CharField(unique=True, max_length=12)
+	surname = models.CharField(max_length=100)
+	name = models.CharField(max_length=100)
+	patronymic = models.CharField(max_length=100)
+	date_of_birth  = models.DateField()
+	gender = models.CharField(choices=GENDER, max_length=10, default='none')
+	city = models.CharField(choices=CITY, max_length=20, default='perm')
+	district = models.CharField(max_length=20, choices=DISTRICT)
+	street = models.CharField(max_length=20)
+	building = models.CharField(max_length=20)
+	room = models.IntegerField()
+	invited_by = models.ForeignKey('self')
+	invite_date = models.DateTimeField(auto_now=False, auto_now_add=True)
+	is_active = models.BooleanField(default=True)
+	penalty = models.IntegerField()
+	def __unicode__(self):
+        	return self.username
+
+class UserAdmin(admin.ModelAdmin):
+	list_display = ('username', 'phone', 'surname', 'name', 'patronymic')
+
+admin.site.register(User, UserAdmin)
